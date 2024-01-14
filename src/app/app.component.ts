6559c9b3dd6d4c15f2b70d5ee650eac2d8899ab7
Ronald Cifuentes <ronald.cifuentes2020@gmail.com>
2024-01-14 01:25:55 -0500

    ✨ feat: add calculator and angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatSlideToggleModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-2';
+  n1: number = 0;
+  n2: number = 0;
+  result: number = 0;
+
+  statusForm = false;
+
+  sumar = () => {
+    this.result = this.n1 + this.n2;
+  };
+  restar = () => {
+    this.result = this.n1 - this.n2;
+  };
 }
